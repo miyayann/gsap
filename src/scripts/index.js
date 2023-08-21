da4b5c97ec6687a60abb8b16f9c26c19941ea017
miya <sirotumesirokuma@gmail.com>
2023-08-21 23:29:40 +0900

    commit

---
+++ src/scripts/index.js
@@ -0,0 +1,3 @@
+import './scripts/main.js';
+import './scripts/sub.js';
+import './scripts/components';
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,35 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
-import { defineConfig } from 'vite';
+const root = "src";
 
 export default defineConfig({
+  root,
+  base: "/",
+  publicDir: "../public",
+  resolve: {
+    alias: [
+      {
+        find: "#",
+        replacement: "/scripts",
+      },
+    ],
+  },
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        // htmlを追加する場合にはこちらに追記
+        index: resolve(root, "index.html"),
+        access: resolve(root, "access.html"),
+        guide: resolve(root, "guide.html"),
+        about: resolve(root, "about.html"),
+        price: resolve(root, "price.html"),
+        staff: resolve(root, "staff.html"),
+      },
+    },
+  },
+  server: {
+    host: true,
+  },
 });
